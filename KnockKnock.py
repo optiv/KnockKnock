f1cfc117546c7fd7769939c1287957fee1274557
James P <23007704+waffl3ss@users.noreply.github.com>
2023-12-09 23:51:17 -0600

    Update KnockKnock.py
    Add threading to OneDrive and fixed some bugs

--- KnockKnock.py
+++ KnockKnock.py
@@ -11,7 +11,7 @@ banner = """
  | ' /| '_ \ / _ \ / __| |/ / ' /| '_ \ / _ \ / __| |/ /
  | . \| | | | (_) | (__|   <| . \| | | | (_) | (__|   <
  |_|\_\_| |_|\___/ \___|_|\_\_|\_\_| |_|\___/ \___|_|\_\\
-   v0.9.1                           Author: @waffl3ss \n\n"""
+    v0.9.5                                  @waffl3ss \n\n"""
 print(banner)
 
 parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
@@ -46,121 +46,86 @@ legacyNames = []
 for name in args.inputList.readlines():
     inputNames.append(name)
 
-def OneDriveEnumerator():
-    tenantData = f"""<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:exm="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:ext="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-            <soap:Header>
-                <a:Action soap:mustUnderstand="1">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action>
-                <a:To soap:mustUnderstand="1">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To>
-                <a:ReplyTo>
-                    <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
-                </a:ReplyTo>
-            </soap:Header>
-            <soap:Body>
-                <GetFederationInformationRequestMessage xmlns="http://schemas.microsoft.com/exchange/2010/Autodiscover">
-                    <Request>
-                        <Domain>{args.targetDomain}</Domain>
-                    </Request>
-                </GetFederationInformationRequestMessage>
-            </soap:Body>
-        </soap:Envelope>"""
-
-    tenantHeaders = {
-        "Content-Type": "text/xml; charset=utf-8",
-        "SOAPAction": '"http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation"',
-        "User-Agent": "AutodiscoverClient",
-        "Accept-Encoding": "identity",
-        }
-
+def OneDriveEnumerator(targetTenant, potentialNameOD):
     try:
-        tenantRequest = requests.post("https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc",headers=tenantHeaders,data=tenantData)
-        tenantRE = re.compile(r"<Domain>([^<>/]*)</Domain>", re.I)
-        tenantDomainList = list(set(tenantRE.findall(tenantRequest.text)))
-        for domain in tenantDomainList:
-            if domain.lower().endswith(".onmicrosoft.com"):
-                targetTenant = domain.split(".")[0]
-    except Exception as e:
-        print("[!] Error retrieving tenant for target, Exiting...")
+        testURL = "https://" + targetTenant + "-my.sharepoint.com/personal/" + str(potentialNameOD.replace(".","_")) + "_" + str(args.targetDomain.replace(".","_")) + "/_layouts/15/onedrive.aspx"
         if args.verboseMode:
-            print("[V] " + str(e))
-        sys.exit()
-
-    if args.verboseMode:
-        print("[V] Using target tenant %s" % targetTenant)
-
-    for potentialUser in nameList:
-        try:
-            testURL = "https://" + targetTenant + "-my.sharepoint.com/personal/" + str(potentialUser.replace(".","_")) + "_" + str(args.targetDomain.replace(".","_")) + "/_layouts/15/onedrive.aspx"
-            if args.verboseMode:
-                print("[V] Testing: " + str(testURL))
-            userRequest = requests.get(testURL, verify=False)
-            if userRequest.status_code in [200, 401, 403, 302]:
-                print("[+] " + str(str(potentialUser) + "@" + str(args.targetDomain)))
-                validNames.append(str(potentialUser))
-            else:
-                if args.verboseMode:
-                    print("[-] " + str(str(potentialUser) + "@" + str(args.targetDomain)))
-                pass
-
-        except Exception as e:
-            if args.verboseMode:
-                print("[V] " + str(e))
-            pass
-
-def teamsEnum(potentialUserNameTeams):
-    if args.verboseMode:
-        print("[V] Testing user %s" % potentialUserNameTeams)
-
-    URL_TEAMS = "https://teams.microsoft.com/api/mt/emea/beta/users/"
-    CLIENT_VERSION = "27/1.0.0.2021011237"
-
-    if Path(args.teamsToken).is_file():
-        tokenFile = open(args.teamsToken, 'r')
-        theToken = str(tokenFile.read())
-    else:
-        theToken = str(args.teamsToken)
-
-    if "Bearer" in theToken:
-        theToken = theToken.replace("Bearer%3D","").replace("%26Origin%3Dhttps%3A%2F%2Fteams.microsoft.com","")
-
-    initHeaders = {
-        "Host": "teams.microsoft.com",
-        "Authorization": "Bearer " + theToken.strip(),
-        "X-Ms-Client-Version": str(CLIENT_VERSION),
-    }
-
-    initRequest = requests.get(URL_TEAMS + str(potentialUserNameTeams) + "/externalsearchv3?includeTFLUsers=true", headers=initHeaders)
-    if initRequest.status_code == 403:
-        print("[+] %s" % potentialUserNameTeams)
-        validNames.append(str(potentialUserNameTeams.split("@")[0]))
-
-    elif initRequest.status_code == 404:
-        print("[!] Error with username - %" % str(potentialUserNameTeams))
-
-    elif initRequest.status_code == 200:
-        statusLevel = json.loads(initRequest.text)
-
-        if statusLevel:
-            if "skypeId" in statusLevel[0]:
-                print("[+] %s -- Legacy Skype Detected" % potentialUserNameTeams)
-                validNames.append(str(potentialUserNameTeams.split("@")[0]))
-                legacyNames.append(str(potentialUserNameTeams.split("@")[0]))
-                if args.verboseMode:
-                    print(json.dumps(statusLevel, indent=2))
-
-            else:
-                print("[+] %s" % potentialUserNameTeams)
-                validNames.append(str(potentialUserNameTeams.split("@")[0]))
-                if args.verboseMode:
-                    print(json.dumps(statusLevel, indent=2))
-
+            print("[V] Testing: " + str(testURL))
+        userRequest = requests.get(testURL, verify=False)
+        if userRequest.status_code in [200, 401, 403, 302]:
+            print("[+] " + str(str(potentialNameOD) + "@" + str(args.targetDomain)))
+            validNames.append(str(potentialNameOD))
         else:
             if args.verboseMode:
-                print("[-] %s" % potentialUserNameTeams)
+                print("[-] " + str(str(potentialNameOD) + "@" + str(args.targetDomain)))
+            pass
 
-    elif initRequest.status_code == 401:
-        print("[!] Error with Teams Auth Token... \n\tShutting down threads and Exiting")
-        sys.exit()
+    except Exception as e:
+        if args.verboseMode:
+            print("[V] " + str(e))
+        pass
+
+def teamsEnum(potentialUserNameTeams):
+    try:
+        if args.verboseMode:
+            print("[V] Testing user %s" % potentialUserNameTeams)
+
+        URL_TEAMS = "https://teams.microsoft.com/api/mt/emea/beta/users/"
+        CLIENT_VERSION = "27/1.0.0.2021011237"
+
+        if Path(args.teamsToken).is_file():
+            tokenFile = open(args.teamsToken, 'r')
+            theToken = str(tokenFile.read())
+        else:
+            theToken = str(args.teamsToken)
+
+        if "Bearer" in theToken:
+            theToken = theToken.replace("Bearer%3D","").replace("%26Origin%3Dhttps%3A%2F%2Fteams.microsoft.com","")
+
+        initHeaders = {
+            "Host": "teams.microsoft.com",
+            "Authorization": "Bearer " + theToken.strip(),
+            "X-Ms-Client-Version": str(CLIENT_VERSION),
+        }
+
+        initRequest = requests.get(URL_TEAMS + str(potentialUserNameTeams) + "/externalsearchv3?includeTFLUsers=true", headers=initHeaders)
+        if initRequest.status_code == 403:
+            print("[+] %s" % potentialUserNameTeams)
+            validNames.append(str(potentialUserNameTeams.split("@")[0]))
+
+        elif initRequest.status_code == 404:
+            if args.verboseMode:
+                print("[!] Error with username - %s" % str(potentialUserNameTeams))
+
+        elif initRequest.status_code == 200:
+            statusLevel = json.loads(initRequest.text)
+
+            if statusLevel:
+                if "skypeId" in statusLevel[0]:
+                    print("[+] %s -- Legacy Skype Detected" % potentialUserNameTeams)
+                    validNames.append(str(potentialUserNameTeams.split("@")[0]))
+                    legacyNames.append(str(potentialUserNameTeams.split("@")[0]))
+                    if args.verboseMode:
+                        print(json.dumps(statusLevel, indent=2))
+
+                else:
+                    print("[+] %s" % potentialUserNameTeams)
+                    validNames.append(str(potentialUserNameTeams.split("@")[0]))
+                    if args.verboseMode:
+                        print(json.dumps(statusLevel, indent=2))
+
+            else:
+                if args.verboseMode:
+                    print("[-] %s" % potentialUserNameTeams)
+
+        elif initRequest.status_code == 401:
+            print("[!] Error with Teams Auth Token... \n\tShutting down threads and Exiting")
+            sys.exit()
+
+    except Exception as e:
+        if args.verboseMode:
+            print("[V] " + str(e))
+        pass        
 
 def main():
     global nameList
@@ -173,30 +138,96 @@ def main():
     nameList = list(dict.fromkeys(nameList))
 
     if args.runOneDrive:
-        if args.verboseMode:
-            print("[V] Running OneDrive Enumeration")
-        OneDriveEnumerator()
+        try:
+            if args.verboseMode:
+                print("[V] Running OneDrive Enumeration")
+
+            tenantData = f"""<?xml version="1.0" encoding="utf-8"?>
+                <soap:Envelope xmlns:exm="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:ext="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+                    <soap:Header>
+                        <a:Action soap:mustUnderstand="1">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action>
+                        <a:To soap:mustUnderstand="1">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To>
+                        <a:ReplyTo>
+                            <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+                        </a:ReplyTo>
+                    </soap:Header>
+                    <soap:Body>
+                        <GetFederationInformationRequestMessage xmlns="http://schemas.microsoft.com/exchange/2010/Autodiscover">
+                            <Request>
+                                <Domain>{args.targetDomain}</Domain>
+                            </Request>
+                        </GetFederationInformationRequestMessage>
+                    </soap:Body>
+                </soap:Envelope>"""
+
+            tenantHeaders = {
+                "Content-Type": "text/xml; charset=utf-8",
+                "SOAPAction": '"http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation"',
+                "User-Agent": "AutodiscoverClient",
+                "Accept-Encoding": "identity",
+                }
+
+            try:
+                tenantRequest = requests.post("https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc",headers=tenantHeaders,data=tenantData)
+                tenantRE = re.compile(r"<Domain>([^<>/]*)</Domain>", re.I)
+                tenantDomainList = list(set(tenantRE.findall(tenantRequest.text)))
+                for domain in tenantDomainList:
+                    if domain.lower().endswith(".onmicrosoft.com"):
+                        targetTenant = domain.split(".")[0]
+            except Exception as e:
+                print("[!] Error retrieving tenant for target, Exiting...")
+                if args.verboseMode:
+                    print("[V] " + str(e))
+                sys.exit()
+
+            if args.verboseMode:
+                print("[V] Using target tenant %s" % targetTenant)
+                print("[V] Running OneDrive Enumeration using %i threads" % args.maxThreads)
+
+            threads = []
+            max_threads = args.maxThreads
+            for potentialNameOD in nameList:
+                while threading.active_count() >= max_threads + 1:
+                    pass
+
+                thread = threading.Thread(target=OneDriveEnumerator, args=(targetTenant, potentialNameOD,))
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
+        except Exception as e:
+            print("[!] Error running OneDrive Enumeration")
+            if args.verboseMode:
+                print("[V] " + str(e))
 
     if args.runOneDrive and args.runTeams:
         nameList = [i for i in nameList if i not in validNames]
 
     if args.runTeams:
-        if args.verboseMode:
-            print("[V] Running Teams User Enumeration using %i threads" % args.maxThreads)
+        try:
+            if args.verboseMode:
+                print("[V] Running Teams User Enumeration using %i threads" % args.maxThreads)
 
-        threads = []
-        max_threads = args.maxThreads
-        for potentialUserNameTeams in nameList:
-            teamsEnumUser = str(potentialUserNameTeams.strip()) + str("@") + str(args.targetDomain)
-            while threading.active_count() >= max_threads + 1:
-                pass
+            threads = []
+            max_threads = args.maxThreads
+            for potentialUserNameTeams in nameList:
+                teamsEnumUser = str(potentialUserNameTeams.strip()) + str("@") + str(args.targetDomain)
+                while threading.active_count() >= max_threads + 1:
+                    pass
 
-            thread = threading.Thread(target=teamsEnum, args=(teamsEnumUser,))
-            thread.start()
-            threads.append(thread)
+                thread = threading.Thread(target=teamsEnum, args=(teamsEnumUser,))
+                thread.start()
+                threads.append(thread)
 
-        for thread in threads:
-            thread.join()
+            for thread in threads:
+                thread.join()
+
+        except Exception as e:
+            print("[!] Error running Teams Enumeration")
+            if args.verboseMode:
+                print("[V] " + str(e))
 
     if validNames:
         if args.outputfile != '':
